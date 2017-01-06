ea52060091448693d729103f03906a1cee50a5da
Aymeric <aymeric.gioia@hotmail.fr>
2017-01-06 15:36:34 +0100

    first co

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// good luck !
+//Ca fonctionne => changement couleur
+ var cocher = [":checkbox"]; 
+ $(":checkbox").click(function(){
+ 	$("p").css("color", "red");
+ 	console.log(":checkbox");
+ });
+
+
+$(".color").click(function(){
+	var color = ($(this).attr("data-color"));
+	console.log (color);
+	$(this).css("background-color", color);
+});
+
+//   var cocher = $("[type = 'checkbox']");
+
+//   cocher.click(function(){
+//   if ($(":checkbox").is("checked")){
+//   	$(".main").css("color", "red");
+//   	console.log(":checkbox");
+// }else{
+// 	$(".main").css("color", "black");
+// }
+// });
